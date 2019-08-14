d722c57faf7be438bbe291cf2528c8d936df92e3
Max Kavun <maxkavun@outlook.com>
2019-08-14 09:36:37 +0000

    Check jenkins trigger

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import calculator
 
 def test_multiplication():
 	if calculator.main(2,2) == 4:
-		print("Unit test is OK")
+		print("Unit test is OK/WORKS")
 	else:
 		print("Unit test was FAILED/ERROR")
 test_multiplication()
